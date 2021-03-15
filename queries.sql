052fe9d37c6cab5c6b04e15380bbd6ac6cc009e9
bella <tianqibu@hotmail.com>
2021-03-15 21:02:11 +0000

    Add stored procedure

--- queries.sql
+++ queries.sql
@@ -7,3 +7,20 @@
 -- function
 
 -- stored procedure
+
+DELIMITER //
+CREATE PROCEDURE YoungAnimals() 
+BEGIN
+
+	SELECT animal_name, species_name, animal_DOB
+    FROM 
+    ANIMALS
+    WHERE 
+    animal_DOB 
+    BETWEEN 
+    '2018-01-01' AND '2021-03-11';
+    
+END// 
+DELIMITER ;
+
+CALL YoungAnimals();
