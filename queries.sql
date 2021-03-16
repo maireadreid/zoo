2264b02e6eef5aa8bd7364b4638ea2532ff5a44e
stamir97 <62836957+stamir97@users.noreply.github.com>
2021-03-16 19:11:43 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -5,6 +5,45 @@
 -- subquery 
 
 -- function
+USE ZOO;
+
+-- CREATE VIEW TABLE THAT COMBINES FOOD TYPE AND AMOUNT
+CREATE VIEW vw_FoodStock 
+AS SELECT f.FoodType_ID, f.FoodType, f.Amount_eats,
+s.Amount_available
+FROM feeding f, stock s
+WHERE f.FoodType_ID = s.FoodType_ID;
+
+SELECT * from vw_FoodStock;
+
+-- FUNCTION TO SEE IF HAVE ENOUGH STOCK
+DELIMITER //
+CREATE FUNCTION enough_FoodStock(Amount_available FLOAT(2), Amount_eats FLOAT(2))
+RETURNS VARCHAR(20) -- 'YES' 'JUST ENOUGH' 'NO'
+DETERMINISTIC
+
+BEGIN	
+DECLARE enough_stock VARCHAR(20);
+
+IF Amount_available > Amount_eats THEN
+SET enough_stock = 'YES';
+
+ELSEIF (Amount_available = Amount_eats) THEN
+SET enough_stock = 'JUST ENOUGH';
+
+ELSEIF Amount_available < Amount_eats THEN
+SET enough_stock = 'NO';
+
+END IF ;
+
+RETURN(enough_stock);
+
+END//
+DELIMITER ;
+
+SELECT FoodType_ID, FoodType, Amount_eats, Amount_available, enough_stock
+from vw_FoodStock;
+
 
 -- stored procedure
 
