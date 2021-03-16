f906889e198b65ee63a1d878792d509b31ad1e58
maireadreid <72817494+maireadreid@users.noreply.github.com>
2021-03-16 18:19:01 +0000

    Populate zookeeper table

--- populate.sql
+++ populate.sql
@@ -6,6 +6,17 @@ enclosure
 values
 (1, 'penguin parade', 'pool', 'penguin', 'hillside view', '0800-1800');
 
+INSERT INTO zookeeper
+(employee_id, full_name, job_title, hire_date, date_of_birth, gender, allocated_enclosure, contact_number)
+VALUES
+(001, 'Michael Scott', 'Manager', 2007-04-05, 1980-08-10, 'Male', null, '07287476472'),
+(002, 'Dwight Schrute', 'Assistant Manager', 2010-03-29, 1984-10-05, 'Male', null, '07485484372'),
+(003, 'Pam Beesly', 'Zookeeper', 2009-06-29, 1992-04-16, 'Female', 'monkey kingdom', '07289287937'),
+(004, 'Jim Halpert', 'Lead Zookeeper', 2013-09-03, 1990-07-19, 'Male', 'monkey kingdom', '07108918872'),
+(005, 'Creed Bratton', 'Zookeeper', 2000-01-01, 1960-12-12, 'Male', 'rhino reserve', null),
+(006, 'Toby Flenderson', 'Zookeeper', 2011-02-19, 1976-02-27, 'Male', 'penguin parade', '07465399100'),
+(007, 'Kelly Kapoor', 'Zookeeper', 2015-03-10, 1995-05-21, 'Female', 'penguin parade', '07773645232');
+
 INSERT INTO 
 ANIMALS
 (animal_id, animal_name, species_name, animal_DOB, arrival_date)
