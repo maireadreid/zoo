a0a8dfad89c5b004719266b1133eba2cd4967516
tianqibu <79051888+tianqibu@users.noreply.github.com>
2021-03-20 10:21:50 +0000

    Update create.sql

--- create.sql
+++ create.sql
@@ -12,12 +12,12 @@ USE zoo;
 
 CREATE TABLE animals
 (
-    animal_id INT NOT NULL,
-    animal_name VARCHAR(55) NOT NULL,
-    species_name VARCHAR(55) NOT NULL,
-    animal_DOB DATE,
-    arrival_date DATE NOT NULL,
-    CONSTRAINT animal_id_pk PRIMARY KEY(animal_id)
+        animal_id INT NOT NULL,
+        animal_name VARCHAR(55) NOT NULL,
+        species_name VARCHAR(55) NOT NULL,
+        animal_DOB DATE,
+        arrival_date DATE NOT NULL,
+        CONSTRAINT animal_id_pk PRIMARY KEY(animal_id)
 );
 
 -- Zookeeper table
@@ -64,10 +64,10 @@ CREATE TABLE feeding
 (
 	foodtype_ID INT,
 	foodtype VARCHAR(50),
-    animal_id INT,
+   	animal_id INT,
 	amount_eats FLOAT(2),
 	CONSTRAINT feeding_pk PRIMARY KEY (foodtype_ID),
-    CONSTRAINT fk_animal_id FOREIGN KEY (animal_id) REFERENCES animals (animal_id)
+   	CONSTRAINT fk_animal_id FOREIGN KEY (animal_id) REFERENCES animals (animal_id)
 );
 
 -- Stock table
@@ -75,8 +75,8 @@ CREATE TABLE feeding
 CREATE TABLE stock 
 (
 	foodtype_ID INT,
-    amount_available Float(2),
-    CONSTRAINT fk_foodtype_ID FOREIGN KEY (foodtype_ID) REFERENCES feeding (foodtype_ID)
+        amount_available Float(2),
+        CONSTRAINT fk_foodtype_ID FOREIGN KEY (foodtype_ID) REFERENCES feeding (foodtype_ID)
 );
 					   
 -- People table
@@ -84,13 +84,13 @@ CREATE TABLE stock
 CREATE TABLE people
 (
 	person_id INT NOT NULL,
-    full_name VARCHAR(55) NOT NULL,
-    address VARCHAR (100),
-    email_address VARCHAR(55),
-    contact_number VARCHAR(55),
-    date_of_birth DATE NOT NULL,
-    gender VARCHAR(10),
-    CONSTRAINT person_id_pk PRIMARY KEY(person_id)
+    	full_name VARCHAR(55) NOT NULL,
+     	address VARCHAR (100),
+    	email_address VARCHAR(55),
+    	contact_number VARCHAR(55),
+   	date_of_birth DATE NOT NULL,
+    	gender VARCHAR(10),
+    	CONSTRAINT person_id_pk PRIMARY KEY(person_id)
 	-- CONSTRAINT person_id_fk FOREIGN KEY(full_name) REFERENCES zookeeper(full_name)
     );
 
@@ -100,10 +100,10 @@ CREATE TABLE visitor_ticket
 (
 	ticket_number_id INT NOT NULL,
 	ticket_number INT NOT NULL,
-    ticket_type VARCHAR(25),
-    date_valid DATE,
-    full_name VARCHAR(55) NOT NULL,
-    CONSTRAINT visitor_ticket_pk PRIMARY KEY(ticket_number_id)
+    	ticket_type VARCHAR(25),
+    	date_valid DATE,
+    	full_name VARCHAR(55) NOT NULL,
+    	CONSTRAINT visitor_ticket_pk PRIMARY KEY(ticket_number_id)
 	-- CONSTRAINT visitor_ticket_fk FOREIGN KEY(ticket_number) REFERENCES visitors(ticket_number)
 );
 
@@ -111,14 +111,14 @@ CREATE TABLE visitor_ticket
 
 CREATE TABLE visitors
 (
-	visitor_id INT NOT NULL,
-	ticket_number INT NOT NULL,
-    full_name VARCHAR(55) NOT NULL,
-    returner BOOLEAN NOT NULL,
-    address VARCHAR(100),
-    contact_number VARCHAR(55),
-    email_address VARCHAR(55),
-    date_of_birth DATE,
-    CONSTRAINT visitors_pk PRIMARY KEY(visitor_id)
-   -- CONSTRAINT visitors_fk FOREIGN KEY(full_name) REFERENCES people(full_name)
+       visitor_id INT NOT NULL,
+       ticket_number INT NOT NULL,
+       full_name VARCHAR(55) NOT NULL,
+       returner BOOLEAN NOT NULL,
+       address VARCHAR(100),
+       contact_number VARCHAR(55),
+       email_address VARCHAR(55),
+       date_of_birth DATE,
+       CONSTRAINT visitors_pk PRIMARY KEY(visitor_id)
+       -- CONSTRAINT visitors_fk FOREIGN KEY(full_name) REFERENCES people(full_name)
 );
