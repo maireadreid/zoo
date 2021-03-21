656086509c95e7a9a6b91a05a820969ac1c4dfbc
stamir97 <62836957+stamir97@users.noreply.github.com>
2021-03-21 13:11:21 +0200

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,6 +1,14 @@
 -- selects
 
 -- joins
+-- CREATE TABLE THAT ADDS FOOD TYPE AND AND AMOUNT
+SELECT f.FoodType_ID, f.FoodType, f.Amount_eats,
+s.Amount_available
+FROM feeding f
+INNER JOIN stock s
+ON
+f.FoodType_ID = s.FoodType_ID;
+SELECT * from food_stock;
 
 -- subquery 
 
